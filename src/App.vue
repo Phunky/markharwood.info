a58765f5d338ded7a38e8cc713002ac62898adef
Mark Harwood <mark@phunky.co.uk>
2018-04-16 17:02:42 +0100

    Playing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div class="m-6 p-8 bg-grey-lighter overflow-y-scroll w-screen font-serif">
+    <img src="./assets/mug.jpg">
     <router-view/>
   </div>
 </template>
 
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+@tailwind preflight;
+@tailwind components;
+@tailwind utilities;
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+body
+  display flex
+  overflow hidden
+  width 100vw
+  height 100vh
 </style>
