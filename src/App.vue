751de40eece447fb68dd8d11a7d51e473e085dc3
Mark Harwood <mark@phunky.co.uk>
2018-04-18 16:54:50 +0100

    More tweaking of crap that does not matter

--- src/App.vue
+++ src/App.vue
@@ -51,8 +51,8 @@
     font-size .8rem
     text-align center
   .avatar 
-    width 125px
-    margin 16px 0
+    width 130px
+    margin-bottom 24px
     border-radius 2em
     border 5px solid #fff
     align-self center
