c8d894c0fb33b00185aabbb81a329b6e3d099d2f
Mark Harwood <mark@phunky.co.uk>
2018-04-19 16:17:25 +0100

    Add changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="about">
-    <p>I'm a well versed software engineer with over fifteen years of experience building web applications and growing start-ups.</p>
-    <p>Constantly striving to push the boundaries of how both businesses and individuals interact with the web on a daily basis.</p>
+    <h1>Hey!</h1>
+    <h3>I'm Mark, a well versed product director and developer with over fifteen years experience of building web applications, automating business processes and growing start-ups.</h3>
+    <h4>Currently working with <a href="https://www.fleetondemand.com" target="_blank">Fleetondemand</a> as the Product Director of <a href="https://www.mobilleo.co" target="_blank">Mobilleo</a> which is the first mobility-as-a-service business travel platform.</h4>
+    <hr />
+    <p>Previously Engineering Manager at <a href="https://www.trendmicro.com" target="_blank">Trend Micro</a>, User Interface Team Lead at <a href="https://www.crunchbase.com/organization/humyo" target="_blank">Humyo.com</a> and other senior and consultant positions more of which you can find on <a href="https://www.linkedin.com/in/markphunkyharwood/" target="_blank">LinkedIn</a></p>
   </div>
 </template>
 
@@ -10,10 +13,17 @@
     margin 2vw 12vw 2vw 4vw
     font-size 2vw
     letter-spacing .05em
-    align-self flex-end
+    align-self flex-start
+  p
+    font-size 1.2rem
   img 
     border-radius 1em
     border .5em solid #f8fafc
   p + p
     margin-bottom 0
+  hr
+    height 0
+    margin 2em 0
+    text-align center
+    border-top 4px solid #DAE1E7
 </style>
