353de8b4ae05f845a7c39bd7eb41dd99497bf4a9
Mark Harwood <mark@phunky.co.uk>
2019-04-09 23:32:39 +0100

    Bloody case sensitive os bs

---
+++ src/components/Twinkle.vue
@@ -0,0 +1,51 @@
+<template>
+  <component :is="tag" :style="color" class="twinkle"><slot /></component>
+</template>
+
+<script>
+export default {
+  name: "twinkle",
+  props: {
+    tag: {
+      type: String,
+      default: "span"
+    },
+    speed: {
+      type: Number,
+      default: 1500
+    }
+  },
+  data() {
+    return {
+      color: null
+    };
+  },
+  created() {
+    this.color = this.randomColor();
+    setInterval(() => {
+      this.color = this.randomColor();
+    }, this.speed);
+  },
+  methods: {
+    randomColor() {
+      let values = [
+        "#E3342F",
+        "#F6993F",
+        "#FFED4A",
+        "#38C172",
+        "#4DC0B5",
+        "#3490DC",
+        "#6574CD",
+        "#9561E2",
+        "#F66D9B"
+      ];
+      return values[Math.floor(Math.random() * values.length)];
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.twinkle
+  transition all .5s ease-in-out
+</style>
