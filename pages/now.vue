87a578cc147bbaad9deafc77e137620adaf921ba
Mark Harwood <mark@phunky.co.uk>
2020-06-21 23:11:32 +0100

    Add some content to /now

--- pages/now.vue
+++ pages/now.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
     <h1>
-      Now
+      What I'm doing right now.
     </h1>
-    <h2 class="mb-4 text-gray-600">
-      This is my <a href="https://nownownow.com/about" target="_blank">nownownow</a> page.
+    <h6 class="text-gray-600">
+      This is my <a href="https://nownownow.com/about" target="_blank">nownownow</a> page, I'm going to do my best to keep this updated but can't promise anything.
+    </h6>
+    <h2>
+      <strong>Last updated;</strong> Sunday 21st June 2020
     </h2>
-    <div class="text-md">
-      <h3 class="text-lg font-bold">
-        Week 25
-      </h3>
-      <ul class="list-inside list-disc">
-        <li>Switched site over to <a href="https://nuxtjs.org/" target="_blank">nuxt.js</a>.</li>
-        <li>Added this <a href="https://nownownow.com/about" target="_blank">now</a> page.</li>
-        <li>Moved CI/CD from AWS CodeBuild to Github Actions.</li>
-      </ul>
-    </div>
+    <p>So we're on week 13 of lockdown due to the Coronavirus pandemic and I'm currently furloughed from my day job and I'm starting to go stir-crazy with the change of routine. However it has allowed me to put a bit of focus into getting my personal site back up and running, which I guess is something...</p>
+    <h2>Manifesting</h2>
+    <ul>
+      <li>Kick start Made by Phunky and start Freelancing/Consulting again.</li>
+      <li>Get up at 6am and run daily.</li>
+      <li>Actively blog on here more often.</li>
+      <li>Finishing a goddamn book.</li>
+    </ul>
   </div>
 </template>
+
+<style>
+  h6, h2, p {
+    @apply mb-4;
+  }
+</style>
