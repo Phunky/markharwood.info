174f2b8915f2c93fc2eb9dd5b8cf337fafb7e77f
Mark Harwood <mark@phunky.co.uk>
2020-06-21 23:12:39 +0100

    CSS Tweaks

--- pages/about.vue
+++ pages/about.vue
@@ -3,9 +3,9 @@
     <h1>
       About
     </h1>
-    <h2 class="text-gray-600">
+    <h6 class="text-gray-600">
       TL;DR: Husband; Father; Gamer; Computer Geek; Striving to be the best version of myself.
-    </h2>
+    </h6>
     <div class="text-md my-4">
       ...
     </div>
