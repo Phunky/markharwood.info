b0bc7d97173e05c8b6052091bbbb0bfb1a0c117f
Mark Harwood <mark@phunky.co.uk>
2020-06-21 22:48:11 +0100

    Add some content to /uses

--- pages/uses.vue
+++ pages/uses.vue
@@ -3,11 +3,47 @@
     <h1>
       Uses
     </h1>
-    <h2 class="text-gray-600">
+    <h6 class="text-gray-600 mb-4">
       This is my <a href="https://uses.tech/" target="_blank">/uses</a> page, be sure to checkout what other people use over at <a href="https://uses.tech/" target="_blank">uses.tech</a>.
-    </h2>
-    <div class="text-md my-4">
-      ...
-    </div>
+    </h6>
+
+    <p>I'd like to say that people often reach out and ask me what I use on a daily basis but they don't 🤪 but i'm sharing them with you anyway.</p>
+
+    <h2>Hardware</h2>
+    <ul>
+      <li>
+        I run everything from my <a href="https://www.amazon.co.uk/Apple-MacBook-Retina-Display-15-inch/dp/B07F88JSSP/" target="_blank">MacBook Pro (Retina, 15-inch, Mid 2015)</a>.
+      </li>
+      <li>
+        I doodle and waste time on an <a href="https://www.amazon.co.uk/dp/B07NHQQ776" target="_blank">5th generation iPad Mini</a> with an <a href="https://www.amazon.co.uk/Apple-MU8F2ZM-A-Pencil-Generation/dp/B07K2PK3BV" target="_blank">Apple Pencil</a>.
+      </li>
+      <li>
+        Current mobile of choice is an <a href="https://www.amazon.co.uk/Apple-iPhone-SE-64-Smartphone-Rose-Gold/dp/B01NA6EPEV" target="_blank">64GB Rose Gold iPhone SE</a> which I picked up for £30 from eBay 🤑.
+      </li>
+      <li>
+        I have a <a href="https://www.amazon.co.uk/Philips-499P9H-00-Widescreen-Multimedia/dp/B07MY2DXPH/" target="_blank">49" Phillips 499P9H</a> in the office, but sadly MacOS can't run this at full native resolution.
+      </li>
+    </ul>
+    <h2>Software</h2>
+    <ul>
+      <li>
+        Ever since I switched to MacOS I've leaned heavily on <a href="https://www.alfredapp.com/" target="_blank">Alfred App</a>.
+      </li>
+      <li>
+        <a href="https://manytricks.com/moom/" target="_blank">Moom</a> is my window manager of choice.
+      </li>
+      <li>
+        My editor of choice these days is <a href="https://code.visualstudio.com/insiders/" target="_blank">VSCode Insiders</a> with the <a href="https://marketplace.visualstudio.com/items?itemName=GitHub.github-vscode-theme" target="_blank">Github Light Theme</a>.
+      </li>
+      <li>
+        <a href="https://iterm2.com/" target="_blank">iTerm2</a> is my go to terminal emulator with the <a href="https://github.com/Relaxed-Theme/relaxed-terminal-themes" target="_blank">Relaxed theme</a>. The last few years i've been using ZSH shell and <a href="https://github.com/zplug/zplug" target="_blank">ZPlug</a>.
+      </li>
+    </ul>
   </div>
 </template>
+
+<style>
+  ul {
+    @apply list-inside list-disc mb-4;
+  }
+</style>
