e473f49ddedca35948f6a35d8851c22b7df08165
Mark Harwood <mark@phunky.co.uk>
2020-07-22 22:03:34 +0100

    Add playing watching and reading to home page

--- pages/index.vue
+++ pages/index.vue
@@ -14,8 +14,21 @@
       I’m also available for <em>short</em> freelance development projects if you wish to <a href="mailto:mark@phunky.co.uk?subject=Let's work together">hire me</a>
     </p>
     <hr class="my-10">
+    <h1 class="mb-4 mt-2">🕹 Playing, Watching and Reading.</h1>
+    <div class="pwr">
+      <a href="https://amzn.to/3jxB7tl" target="_blank"><img border="0" src="//ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1786892340&Format=_SL160_&ID=AsinImage&MarketPlace=GB&ServiceVersion=20070822&WS=1&tag=phunky-21&language=en_GB" ></a>
+
+      <a href="https://amzn.to/3hr32t9" target="_blank"><img border="0" src="//ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07S1BKF3G&Format=_SL160_&ID=AsinImage&MarketPlace=GB&ServiceVersion=20070822&WS=1&tag=phunky-21&language=en_GB" ></a>
+
+      <a href="https://amzn.to/2E2jDVM" target="_blank"><img border="0" src="//ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1473661234&Format=_SL160_&ID=AsinImage&MarketPlace=GB&ServiceVersion=20070822&WS=1&tag=phunky-21&language=en_GB" ></a>
+
+      <a href="https://amzn.to/2CH5Qnc" target="_blank"><img border="0" src="//ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07D8DPG27&Format=_SL160_&ID=AsinImage&MarketPlace=GB&ServiceVersion=20070822&WS=1&tag=phunky-21&language=en_GB" ></a>
+
+      <a href="https://amzn.to/3hw1EWp" target="_blank"><img border="0" src="//ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B087Z6TLN8&Format=_SL250_&ID=AsinImage&MarketPlace=GB&ServiceVersion=20070822&WS=1&tag=phunky-21&language=en_GB" ></a>
+    </div>
+    <hr class="my-10">
     <h1 class="mb-4 mt-2">
-      ✍️ I try to write, yet often fail...
+      ✍️ Writing and rambling.
     </h1>
     <div v-for="(entry, key) in entries" :key="key" class="mt-4 mb-8">
       <h6 class="m-0">
@@ -60,3 +73,12 @@ export default {
   }
 }
 </script>
+
+<style>
+.pwr a {
+  @apply inline-block mr-2 border-4;
+}
+.pwr a img {
+  height: 130px;
+}
+</style>
