5263907bf72a8cb396ce9e866a130c7e6e360fb5
Mark Harwood <mark@phunky.co.uk>
2020-07-26 17:15:33 +0100

    Update nownownow content

--- pages/now.vue
+++ pages/now.vue
@@ -7,17 +7,31 @@
       This is my <a href="https://nownownow.com/about" target="_blank">nownownow</a> page, I'm going to do my best to keep this updated but can't promise anything.
     </h6>
     <hr class="mb-4">
-    <h6>
-      Last updated; Sunday 12th July 2020
-    </h6>
-    <p>So we're on week <strong>15</strong> of lockdown due to the Coronavirus pandemic and <span class="line-through">I'm currently furloughed from my day job</span> I've handed in my notice at <a href="https://www.fleetondemand.com" target="_blank">Fleetondemand</a> after <strong>eight years</strong> and looking forward to a fresh start next month.</p>
-    <p>Lockdown is still a bit of a struggle but I think we've all got used to the new way of life now and if I'm honest I really needed the rest as it forced me to reassess my priorities and realign them with what I actually want to achieve.</p>
+    <p>I'm currently preparing for a lot of upcoming changes with the new job starting in August, it's remote based which should enable to produce my best work at a time that fits me. While also allow me to do more of what I enjoy without having to think about commuting, dress codes or office politics.</p>
+    <p><strong>I can't wait!</strong></p>
+    <p>At the start of July I decided that I would build some new habits, one of which was to run daily, something I've said I would do many times in the past but never followed through on. However this time I've done just that and really am starting to enjoy waking up a 6am and falling out of bed for a run.</p>
+    <iframe
+      height="405"
+      width="590"
+      frameborder="0"
+      allowtransparency="true"
+      scrolling="no"
+      src="https://www.strava.com/activities/3813405630/embed/72be58133f686cee0a4c74119b032c6944bb6c04"
+    />
+    <p>I've started writing on here too which is great to see and the plan is to put something out once a week. I would like to do some technical writing but will have to see if inspiration hits me or not.</p>
     <h2>Manifesting</h2>
     <ul>
-      <li>Kick start Made by Phunky and start Freelancing/Consulting again.</li>
-      <li><span class="line-through">Get up at 6am and run daily</span> Been running 2 miles daily for the last 10 days.</li>
-      <li>Actively blog on here more often.</li>
-      <li><span class="line-through">Finishing a goddamn book</span> Read <a href="https://www.lemnsissay.com/" target="_blank">Lemn Sissay</a> memoir <a href="https://amzn.to/2DBO5WH" target="_blank">"My name is Why"</a> cover-to-cover the day it arrived.</li>
+      <li>Pick up at least another freelance side gig before the end of the year.</li>
+      <li>Get back to the Gym and continue with my daily runs.</li>
+      <li>Write on here more often.</li>
+      <li>Finish another book!</li>
+      <li>Finsh <a href="https://amzn.to/3f1bVZ4" target="_blank">Legend of Zelda Link's Awakening</a> on the Switch.</li>
     </ul>
+    <p>I'm thinking of moving this to the homepage in the future as it should be more visible!</p>
+    <hr class="mt-8 mb-4">
+    <div>
+      <h6>Last updated; Sunday 26th July 2020</h6>
+      You can view previous commits of this <a href="https://nownownow.com/about" target="_blank">nownownow</a> page on <a href="https://github.com/Phunky/markharwood.info/commits/production/pages/now.vue" target="_blank">Github</a>
+    </div>
   </div>
 </template>
