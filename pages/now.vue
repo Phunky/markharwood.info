f16b5f7b8dcfead6b6cc1600b145de40d5e644d9
Mark Harwood <mark@phunky.co.uk>
2021-07-08 16:04:16 +0100

    Update the now page, finally.

--- pages/now.vue
+++ pages/now.vue
@@ -7,30 +7,18 @@
       This is my <a href="https://nownownow.com/about" target="_blank">nownownow</a> page, I'm going to do my best to keep this updated but can't promise anything.
     </h6>
     <hr class="mb-4">
-    <p>I'm currently preparing for a lot of upcoming changes with the new job starting in August, it's remote based which should enable to produce my best work at a time that fits me. While also allow me to do more of what I enjoy without having to think about commuting, dress codes or office politics.</p>
-    <p><strong>I can't wait!</strong></p>
-    <p>At the start of July I decided that I would build some new habits, one of which was to run daily, something I've said I would do many times in the past but never followed through on. However this time I've done just that and really am starting to enjoy waking up a 6am and falling out of bed for a run.</p>
-    <iframe
-      height="405"
-      width="590"
-      frameborder="0"
-      allowtransparency="true"
-      scrolling="no"
-      src="https://www.strava.com/activities/3813405630/embed/72be58133f686cee0a4c74119b032c6944bb6c04"
-    />
-    <p>I've started writing on here too which is great to see and the plan is to put something out once a week. I would like to do some technical writing but will have to see if inspiration hits me or not.</p>
+    <p>As predicted this page hasn't been touched since it was launched almost year ago... So I'll keep this short.</p>
     <h2>Manifesting</h2>
     <ul>
-      <li>Pick up at least another freelance side gig before the end of the year.</li>
-      <li>Get back to the Gym and continue with my daily runs.</li>
-      <li>Write on here more often.</li>
-      <li>Finish another book!</li>
-      <li>Finsh <a href="https://amzn.to/3f1bVZ4" target="_blank">Legend of Zelda Link's Awakening</a> on the Switch.</li>
+      <li>Watching <a href="https://uk.finance.yahoo.com/quote/AMC?p=AMC" target="_blank">AMC</a> hoping that the squeeze is a coming.</li>
+      <li>Getting back into my <a href="https://www.strava.com/athletes/60154295" target="_blank">5km daily runs</a>.</li>
+      <li><b>Write on here more often</b>.</li>
+      <li>Started streaming play through videos on <a href="https://www.twitch.tv/irphunky" target="_blank">Twitch</a> and <a href="https://www.youtube.com/c/MarkPhunkyHarwood/featured" target="_blank">Youtube</a></li>
+      <li>Crossing my fingers that England win Euro 2020.</li>
     </ul>
-    <p>I'm thinking of moving this to the homepage in the future as it should be more visible!</p>
     <hr class="mt-8 mb-4">
     <div>
-      <h6>Last updated; Sunday 26th July 2020</h6>
+      <h6>Last updated; Thursday 8th July 2021</h6>
       You can view previous commits of this <a href="https://nownownow.com/about" target="_blank">nownownow</a> page on <a href="https://github.com/Phunky/markharwood.info/commits/production/pages/now.vue" target="_blank">Github</a>
     </div>
   </div>
