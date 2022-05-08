735f4d58ecd2de29a4319548c8c46d23c6c8873a
Mark Harwood <mark@phunky.co.uk>
2022-05-08 20:42:49 +0100

    Update now.vue

--- pages/now.vue
+++ pages/now.vue
@@ -10,15 +10,15 @@
     <p>As predicted this page hasn't been touched since it was launched almost year ago... So I'll keep this short.</p>
     <h2>Manifesting</h2>
     <ul>
-      <li>Watching <a href="https://uk.finance.yahoo.com/quote/AMC?p=AMC" target="_blank">AMC</a> hoping that the squeeze is a coming.</li>
-      <li>Getting back into my <a href="https://www.strava.com/athletes/60154295" target="_blank">5km daily runs</a>.</li>
+      <li>Still watching <a href="https://uk.finance.yahoo.com/quote/AMC?p=AMC" target="_blank">AMC</a>, but now hoping that it's value hits the $30s again so I can recoup some of my losses :(.</li>
+      <li>Stopped my daily runs and doubled down on the gym. Still need a better workout plan than my tweaked 5x5 Stronglifts tho.</li>
       <li><b>Write on here more often</b>.</li>
-      <li>Started streaming play through videos on <a href="https://www.twitch.tv/irphunky" target="_blank">Twitch</a> and <a href="https://www.youtube.com/c/MarkPhunkyHarwood/featured" target="_blank">Youtube</a></li>
-      <li>Crossing my fingers that England win Euro 2020.</li>
+      <li>Playing a lot of <a href="https://www.oculus.com/experiences/quest/5157404804284116/?msclkid=ec141f74cf0611ec812a2b8bc322c01a" target="_blank">Nock</a> on the Oculus Quest</li>
+      <li>Starting divorce proceedings :(</li>
     </ul>
     <hr class="mt-8 mb-4">
     <div>
-      <h6>Last updated; Thursday 8th July 2021</h6>
+      <h6>Last updated; Sunday 8th May 2022</h6>
       You can view previous commits of this <a href="https://nownownow.com/about" target="_blank">nownownow</a> page on <a href="https://github.com/Phunky/markharwood.info/commits/production/pages/now.vue" target="_blank">Github</a>
     </div>
   </div>
